50525cc18c2b94f6dcda2d95497f925d883b7a2e
Anton Guryev <anton-guryev@digitaledge.net>
2018-06-11 16:23:37 -0400

    Add models

---
+++ DemoApp1/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp1.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [MaxLength(200)]
+        public string FirstName { get; set; }
+
+        [MaxLength(200)]
+        public string LastName { get; set; }
+
+    }
+}
