6c299559e443a79a9c470ac3f86008c587f045d4
Anton Guryev <anton-guryev@digitaledge.net>
2018-06-11 16:57:07 -0400

    Switch to .net core 2.1 and add EF

---
+++ DemoApp1/Data/DemoApp1Context.cs
@@ -0,0 +1,19 @@
+﻿using DemoApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DemoApp1.Data
+{
+    public class DemoApp1Context : DbContext
+    {
+        public DemoApp1Context(DbContextOptions<DemoApp1Context> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
